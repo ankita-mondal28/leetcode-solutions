99aa9c688ab2d455e67c227db55e622f22155845
Ankita <ankitamondal2801@gmail.com>
2026-07-15 19:22:55 +0530

    Time: 56 ms (25.39%), Space: 58.2 MB (5.08%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,11 @@
+function createCounter(init) {
+    let current = init;
+    return {
+        increment: () => ++current,
+        decrement: () => --current,
+        reset: () => {
+            current = init;
+            return current;
+        }
+    };
+}
